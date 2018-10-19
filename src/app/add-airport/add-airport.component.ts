ef1c87b6c62f22f41f61b59e04461ebb2abbca28
Wouter Pen <penwouter@gmail.com>
2018-10-19 08:46:20 +0200

    added components added functionality

---
+++ src/app/add-airport/add-airport.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { AirportServiceService } from '../services/airport-service.service';
+
+
+@Component({
+  selector: 'app-add-airport',
+  templateUrl: './add-airport.component.html',
+  styleUrls: ['./add-airport.component.css']
+})
+export class AddAirportComponent implements OnInit {
+
+  constructor(private airportService: AirportServiceService) { }
+
+  ngOnInit() {
+  }
+
+  addAirport(airportForm) {
+    const airportAddForm = {
+      'id': 0,
+      'name':airportForm.value.airport
+    }
+    this.airportService.addAirport(airportAddForm).subscribe(
+      data => console.log('data',data)
+    )
+  }
+
+}
