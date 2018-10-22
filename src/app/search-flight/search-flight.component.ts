f1f16455ec875ebb92890a1ffcaa26bf260ffad6
Balraj Singh <B.Singh91@hotmail.com>
2018-10-22 12:06:35 +0200

    Search screen added

---
+++ src/app/search-flight/search-flight.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-flight',
+  templateUrl: './search-flight.component.html',
+  styleUrls: ['./search-flight.component.css']
+})
+export class SearchFlightComponent implements OnInit {
+
+
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  selectedFlight(searchForm) {
+    var searchFlightDetails = {
+      'from': searchForm.value.from,
+      'to': searchForm.value.to,
+      'departure' : searchForm.value.departureDate,
+      'return' : searchForm.value.returnDate,
+      'passengers' : searchForm.value.passengers,
+    }
+  }
+
+
+
+}
